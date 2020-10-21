e2d192cca8440e27c77ec37294e0f626cef4af19
Vlad Grigorescu <vlad@es.net>
2020-10-21 13:23:00 -0500

    Try to have a job that summarizes everything

--- debug.sh
+++ debug.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PKG_NAME=$1
+
 if compgen -G "$HOME"/.zkg/logs/*-build.log > /dev/null; then
     for i in "$HOME"/.zkg/logs/*-build.log; do
         echo "Build failed"
@@ -7,6 +9,8 @@ if compgen -G "$HOME"/.zkg/logs/*-build.log > /dev/null; then
         echo "${i}:"
         cat "$i"
     done
+    echo "::set-output name=results::{\"$PKG_NAME\": {\"status\": \"failed\", \"stage\": \"build\"}}"
+    exit 1
 fi
 
 if compgen -G "$HOME"/.zkg/testing/*/clones/*/zkg.test_command.{stderr,stdout} > /dev/null; then
@@ -16,6 +20,6 @@ if compgen -G "$HOME"/.zkg/testing/*/clones/*/zkg.test_command.{stderr,stdout} >
         echo "${i}:"
         cat "$i"
     done
+    echo "::set-output name=results::{\"$PKG_NAME\": {\"status\": \"failed\", \"stage\": \"test\"}}"
+    exit 1
 fi
-
-exit 1
